16af58af6db49fd4d7088ff33112e9af761201d0
MIKE-SLAYER\mikes <metamike94@gmail.com>
2017-03-23 02:01:38 -0600

    inicio del proyecto

---
+++ template/navbar.php
@@ -0,0 +1,11 @@
+<div class="navbar-collapse collapse ">
+    <ul id="menu-top" class="nav navbar-nav navbar-right">
+        <li><a class="menu-top-active" href="../home/index.php">Dashboard</a></li>
+        <li><a href="../ui.html">UI Elements</a></li>
+        <li><a href="../table.html">Data Tables</a></li>
+        <li><a href="../forms.html">Forms</a></li>
+         <li><a href="../login.html">Login Page</a></li>
+        <li><a href="../blank.html">Blank Page</a></li>
+
+    </ul>
+</div>
